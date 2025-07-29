818c4d66c0e7fc5305077b87ecebad241f554f49
Corentin M <DeadStorm19@gmail.com>
2025-07-29 15:05:41 +0200

    Updated action, added documentation

--- src/action.hpp
+++ src/action.hpp
@@ -8,6 +8,13 @@ struct Action
     const std::string type{"NONE"};
 
     explicit Action() noexcept = default;
+
+    /**
+     * @brief Create an action
+     * 
+     * @param name Action's name
+     * @param type Action's type
+     */
     explicit Action(std::string name, std::string type) noexcept
         : name(std::move(name)), type(std::move(type))
     {
