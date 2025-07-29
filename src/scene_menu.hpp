3bf22bacd3695c061ffc99a8ab8e1a8895302ae2
Corentin M <DeadStorm19@gmail.com>
2025-07-29 15:31:03 +0200

    Updated scene menu, added documentation, separated attributes and methods

--- src/scene_menu.hpp
+++ src/scene_menu.hpp
@@ -6,8 +6,38 @@
 class SceneMenu : public Scene
 {
 public:
+    /**
+     * @brief Create a Menu Scene
+     *
+     * @param game Pointer to the GameEngine
+     */
     explicit SceneMenu(GameEngine *game = nullptr);
+
+    /**
+     * @brief Render the scene
+     */
     void system_render() noexcept override;
+
+private:
+    /**
+     * @brief Initialize the scene (store actions, load fonts, make level selection etc.)
+     */
+    void init();
+
+    /**
+     * @brief Update the scene
+     */
+    void update() noexcept override;
+
+    /**
+     * @brief Handle an action
+     */
+    void system_do_action(const Action &action) noexcept override;
+
+    /**
+     * @brief Handle the scene exit
+     */
+    void on_end() noexcept override;
 
 private:
     std::string m_title{};
@@ -17,9 +47,4 @@ private:
     std::vector<std::string> m_menu_strings{};
     std::vector<std::string> m_level_paths{};
     size_t m_selected_menu_index{};
-
-    void init();
-    void update() noexcept override;
-    void system_do_action(const Action &action) noexcept override;
-    void on_end() noexcept override;
 };
