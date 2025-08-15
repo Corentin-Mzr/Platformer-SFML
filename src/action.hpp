b8313e8aa9605e815ea96f3f2beb2e735b7e91ac
Corentin M <DeadStorm19@gmail.com>
2025-08-15 16:14:15 +0200

    Added documentation to Action

--- src/action.hpp
+++ src/action.hpp
@@ -2,18 +2,26 @@
 
 #include <string>
 
+/**
+ * @brief Represents an action the user can do in the game depending on a key state
+ * 
+ * @note type is either START (key pressed) or END (key released)
+ */
 struct Action
 {
     const std::string name{"NONE"};
     const std::string type{"NONE"};
 
+    /**
+     * @brief Default constructor
+     */
     explicit Action() noexcept = default;
 
     /**
      * @brief Create an action
      * 
      * @param name Action's name
-     * @param type Action's type
+     * @param type Action's type (START when key pressed, END when key released)
      */
     explicit Action(std::string name, std::string type) noexcept
         : name(std::move(name)), type(std::move(type))
